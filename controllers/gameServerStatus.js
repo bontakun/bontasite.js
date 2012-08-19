db712f6d9fc74f7727d7f3cb1182de0ee5d61cf9
Benjamin Dow <ben@bonta-kun.net>
2012-08-18 22:57:23 -0500

    Added error handling and updated TF2 header.

--- controllers/gameServerStatus.js
+++ controllers/gameServerStatus.js
@@ -2,18 +2,37 @@ module.exports = {
 	request: request
 };
 
-function responseCallBack(response, serverInfo) {
+function responseCallBack(response, serverInfos) {
 	response.render('gameServerStatus', {
         locals: {
             node_server_url: 'http://ve.bonta-kun.net',
             title: "Game Servers",
-            servers: [ serverInfo ]
+            servers: addImagesToServerInfos(serverInfos)
         }
     });
 }
 
 function request(request, response) {
-	require("../models/gameServerStatus.js").getServerStatus('tf.bonta-kun.net', 27015, function(serverInfo) {
-		responseCallBack(response, serverInfo);
+	require("steam-server-status").getServerStatus(
+			'tf.bonta-kun.net', 27015, function(serverInfo) {
+		responseCallBack(response, [ serverInfo ]);
 	});
+}
+
+function addImagesToServerInfos(serverInfos) {
+	for (var i = 0; i < serverInfos.length; i++) {
+		serverInfos[i].image = extractImageFromGameDirectory(serverInfos[i].gameDirectory);
+	}
+	return serverInfos;
+}
+
+function extractImageFromGameDirectory(gameDirectory) {
+	if (gameDirectory == "tf") {
+		return {
+			url: "/images/games/tf2/Crew.jpg",
+			height:"299px",
+			width: "930px"
+		}
+	}
+	return null;
 }
