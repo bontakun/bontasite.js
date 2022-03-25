19786e9f05041aad34660ce0653b340f897daf1e
Benjamin <ben@bontakun.net>
2022-03-25 00:45:17 -0700

    Updates to make run with current versions.

--- app.js
+++ app.js
@@ -1,55 +1,48 @@
 var express = require('express');
-var app = module.exports = express.createServer();
+var app = express();
 
-// Configuration
-app.configure(function() {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'mustache');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-    app.register(".mustache", require('stache'));
-});
+var mustacheExpress = require('mustache-express');
 
-app.configure('development', function() {
-    app.use(express.errorHandler({ 
-        dumpExceptions: true, 
-        showStack: true 
-    }));
-});
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
 
-app.configure('production', function(){
-    app.use(express.errorHandler());
-});
+// Enable partials
+//app.engine('mst', mustache(VIEWS_PATH + '/partials', '.mst'));
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
 // Routes
-var index = require('./controllers/index.js')
+var index = require('./controllers/index.js');
 app.all('/', index.request);
 
 //TODO: clean up this routing
-var blog = require('./controllers/blog.js');
-app.all('/blog/page/:pageNumber', blog.request);
-app.all('/blog/post/:post', blog.postRequest);
-app.all('/blog', blog.request);
+// var blog = require('./controllers/blog.js');
+// app.all('/blog/page/:pageNumber', blog.request);
+// app.all('/blog/post/:post', blog.postRequest);
+// app.all('/blog', blog.request);
 
-var projects = require('./controllers/projects.js');
-app.all('/projects', projects.request);
+// var projects = require('./controllers/projects.js');
+// app.all('/projects', projects.request);
 
-var contact = require('./controllers/contact.js');
-app.all('/contact', contact.request);
+// var contact = require('./controllers/contact.js');
+// app.all('/contact', contact.request);
 
-var about = require('./controllers/about.js');
-app.all('/about', about.request);
+// var about = require('./controllers/about.js');
+// app.all('/about', about.request);
 
-var gameServerStatus = require('./controllers/gameServerStatus.js');
-app.all('/gameServerStatus', gameServerStatus.request);
+// var gameServerStatus = require('./controllers/gameServerStatus.js');
+// app.all('/gameServerStatus', gameServerStatus.request);
 
 //var fourOFour = require('./controllers/404.js');
 //app.all('*', fourOFour.request);
 
-app.listen(7401);
-console.log("Express server listening on port %d", app.address().port);
+app.listen(8080);
+
+app.listen(80);
+app.listen(443);
+console.log("Express server listening.");
 
 process.on('uncaughtException', function(error) {
   console.error("Uncaught exception occurred: " + error);
