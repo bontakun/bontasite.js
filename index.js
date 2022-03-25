6c366da02ac07c6331920038548776dd35fe0e61
Benjamin <ben@bontakun.net>
2022-03-25 01:24:14 -0700

    Having a bit of fun.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+  
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// routes
+app.get('/', (req, res) => {
+
+  res.render('pages/index', {secret: req.query.secret, ip: req.ip })
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
